9e084fecefb5ad6b5c6193345418128d8c747efb
Ziabeher Phillips <zabq02@gmail.com>
2022-05-02 21:20:41 -0400

    added react icons, started hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { AiFillGithub, AiOutlineTwitter } from 'react-icons/ai'
+const Hero = () => {
+  return (
+    <header className="header">
+        <div className="my-links">
+            <AiFillGithub/>
+            <AiOutlineTwitter/>
+        </div>
+
+        <div className="light-switch">
+            
+        </div>
+    </header>
+  )
+}
+
+export default Hero
