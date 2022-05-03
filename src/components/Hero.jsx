16867cbf330c32c055cdf83e2d8d9a79ce0f0c33
Ziabeher Phillips <zabq02@gmail.com>
2022-05-02 22:13:50 -0400

    functioning light switch

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Hero.css'
 import { AiFillGithub, AiOutlineTwitter } from 'react-icons/ai'
+import { BsFillSunFill } from 'react-icons/bs'
+import { BsMoonStarsFill } from 'react-icons/bs'
+
 const Hero = () => {
+
+    // State
+    const [lightToggle, setLightToggle] = useState(false)
+
+    const lightSwitch = () => {
+        document.body.classList.toggle("dark-theme");
+    }
+
   return (
     <header className="header">
         <div className="my-links">
@@ -9,7 +21,12 @@ const Hero = () => {
         </div>
 
         <div className="light-switch">
-            
+            <input type="checkbox" className="checkbox" id="checkbox" onChange={lightSwitch}/>
+            <label htmlFor="checkbox" className='label'>
+                <BsFillSunFill className='sun'/>
+                <BsMoonStarsFill className='moon'/>
+                <div className="ball"></div>
+            </label>
         </div>
     </header>
   )
