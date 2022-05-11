131625156f8c349f1256f11a715f58b3543a54de
Ziabeher Phillips <zabq02@gmail.com>
2022-05-11 13:30:09 -0400

    fixed footer and dark theme

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,10 +11,10 @@ const Footer = () => {
 
       <ul className="permalinks">
         <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Experience</a></li>
-        <li><a href="#">Portfolio</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#tech">Tech</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
 
       <div className="footer__socials">
