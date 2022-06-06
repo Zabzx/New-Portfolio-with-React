86f9d74c611fcf0c7e2768d1479936a01b3e65ba
Ziabeher Phillips <zabq02@gmail.com>
2022-06-06 15:11:09 -0400

    added tags for project items

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -12,35 +12,40 @@ const projectItems = [
     image: FLOWW,
     title: 'Floww',
     github: 'https://github.com/Zabzx/Floww',
-    link: 'https://flowwd.netlify.app/'
+    link: 'https://flowwd.netlify.app/',
+    stack: ['React', 'React Router', 'CSS']
   },
   {
     id: 5,
     image: SPICE,
     title: 'SpiceUp!',
     github: 'https://github.com/Zabzx/spiceUp',
-    link: 'https://spicedup.netlify.app/'
+    link: 'https://spicedup.netlify.app/',
+    stack: ['React', 'React Router', 'Tailwind CSS']
   },
   {
     id: 3,
     image: CLOUDTECH,
     title: 'CloudTech',
     github: 'https://github.com/Zabzx/Static',
-    link: 'https://cloudtechh.netlify.app/'
+    link: 'https://cloudtechh.netlify.app/',
+    stack: ['React', 'CSS',]
   },
   {
     id: 1,
     image: MYTOURNI,
     title: 'MyTourni',
     github: 'https://github.com/Zabzx/tourni',
-    link: 'http://mytourni.com/'
+    link: 'http://mytourni.com/',
+    stack: ['React', 'GSAP', 'CSS']
   },
   {
     id: 4,
     image: DATE,
     title: 'Live Date Countdown',
     github: 'https://github.com/Zabzx/Live-Countdown',
-    link: 'https://livedatecountdown.netlify.app/'
+    link: 'https://livedatecountdown.netlify.app/',
+    stack: ['HTML', 'CSS', 'JavaScript']
   },
 ];
 
@@ -50,16 +55,24 @@ const Projects = () => {
         <h2 className="align-center">My Projects</h2>
         <div className="container portfolio__container">
         {
-          projectItems.map(({id, image, title, github, link}) => {
+          projectItems.map((item) => {
             return (
-              <article key={id} className="project-item">
+              <article key={item.id} className="project-item">
               <div className="project-item-image">
-              <img src={image} alt="" />
+              <img src={item.image} alt="" />
               </div>
-              <h3>{title}</h3>
+              <h3>{item.title}</h3>
               <div className="portfolio__item-cta">
-              <a href={github} className="btn-pf" target="_blank">GitHub</a>
-              <a href={link} className="btn-pf2" target="_blank">Link to App</a>
+              <a href={item.github} className="btn-pf" target="_blank">GitHub</a>
+              <a href={item.link} className="btn-pf2" target="_blank">Link to App</a>
+              </div>
+              <div className="project-tags">
+                <h3>Tags</h3>
+                <ul className="tag-list container">
+                  {item.stack.map((i) => (
+                    <li>{i}</li>
+                  ))}
+                </ul>
               </div>
               </article>
             )
